2171c65774957827f71020479b79d615c762a07f
JonAlex99 <jonalexpetursson@gmail.com>
2020-10-25 18:09:45 +0000

    Vinna í verkefni 7

--- scripts.js
+++ scripts.js
@@ -8,7 +8,64 @@ const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
  * Byrja forrit.
  */
 function start() {
-  alert('Halló!')
+  let input = prompt('Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“');
+  let inputN;
+  let sesarString;
+  if(input.toLocaleUpperCase('is-IS') === 'KÓÐA'){
+    inputN = erNGilt();
+    sesarString = erSesarGilt(input, inputN);
+    alert(encode(sesarString, inputN));
+
+  } else if(input.toLocaleUpperCase('is-IS') === 'AFKÓÐA'){
+    inputN = erNGilt();
+    sesarString = erSesarGilt(input, inputN);
+    alert(decode(sesarString, inputN));
+
+  } else {
+    alert('Veit ekki hvaða aðgerð ' + input + ' er. Reyndu aftur.');
+    start();
+  }
+
+}
+
+function erNGilt(){
+  let inputN = prompt('Hversu mikið á að hliðra strenginn? Gefðu upp heiltölu á bilinu [1, 31]');
+  if(inputN < 32 && inputN > 0){
+    return parseInt(inputN);
+  } else {
+    alert('Veit ekki hvaða aðgerð "' + inputN + '" er. Reyndu aftur.');
+    erNGilt();
+  }
+}
+
+function erSesarGilt(input, inputN){
+  let svar = prompt('Gefðu upp strenginn sem á að ' + input + ' með hliðrun ' + inputN);
+  let vit = erEkkiISL(svar.toLocaleUpperCase('is-IS'));
+  if(svar === ''){
+    alert('Þú gafst ekki upp streng. Reyndu aftur!');
+    erSesarGilt(input,inputN);
+  } else if (vit.length){
+    alert('Þú gafst upp stafi sem ekki er hægt að ' + input + ': ' + '\'' + vit.join(', ') + '\'');
+    erSesarGilt(input,inputN);
+  } else {
+    return svar.toLocaleUpperCase('is-IS');
+  }
+}
+
+
+function erEkkiISL(str){
+  var skil = [];
+  for(let i = 0; i < str.length; i++){
+    if(LETTERS.indexOf(str[i]) === -1){
+      skil.push(str[i]);
+    }
+  }
+  return skil;
+}
+
+function mod(n, m) {
+  console.log(n + ' ' + m);
+  return ((n % m) + m) % m;
 }
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
@@ -22,7 +79,13 @@ start();
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n) {
-  return str;
+  let temp = '';
+  for(let i = 0; i < str.length; i++){
+    console.log(LETTERS.indexOf(str[i]));
+    console.log(n);
+    temp += LETTERS[mod((LETTERS.indexOf(str[i]) + n),32)];
+  }
+  return temp;
 }
 
 /**
@@ -33,7 +96,11 @@ function encode(str, n) {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n) {
-  return str;
+  let temp = '';
+  for(let i = 0; i < str.length; i++){
+    temp += LETTERS[mod((LETTERS.indexOf(str[i]) - n),32)];
+  }
+  return temp;
 }
 
 console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
